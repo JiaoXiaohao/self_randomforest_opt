a9fbfd4e1d4d9a04884d140a357abf457da82db4
JiaoXiaohao <2745602379@qq.com>
2024-04-15 00:04:10 +0800

    Update PlotIMG.py to fix band order in image display

--- PlotIMG.py
+++ PlotIMG.py
@@ -11,7 +11,7 @@ print(dataArray.shape)
 plt.style.use("ggplot")  # 绘制遥感影像
 plt.figure(figsize=(8, 8))
 # 绘制4，3，2波段
-plt.imshow(np.transpose(dataArray[[3, 2, 1], :, :]))
+plt.imshow(np.transpose(dataArray[[4, 3, 2], :, :]))
 plt.axis("off")
 plt.savefig("img.png", dpi=300, bbox_inches="tight")
 plt.show()
